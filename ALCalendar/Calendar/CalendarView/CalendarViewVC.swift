c50bf7dbac1c3c97dcbb9485e2aec9b1599ad480
Sebastian <sebastian.grabinski@applandeo.com>
2018-01-25 11:10:42 +0100

    VC constructor

--- ALCalendar/Calendar/CalendarView/CalendarViewVC.swift
+++ ALCalendar/Calendar/CalendarView/CalendarViewVC.swift
@@ -13,15 +13,17 @@ public class CalendarViewVC: UIViewController, CalendarViewDelegate {
     var calendarView: CalendarView!
     var okButton: UIButton!
     var cancelButton: UIButton!
+    var stackView: UIStackView!
     
-    public var cellEventColor: UIColor!
-    public var cellSelectionType: CalendarStyle.CellSelectionType!
-    public var cellShape : CalendarStyle.CellShapeOptions!
-    public var cellBackgroundColor : UIColor!
-    public var cellBorderColor : UIColor!
-    public var headerTextColor : UIColor!
-    public var cellTextColor : UIColor!
-    public var cellTodayTextColor : UIColor!
+    public var cellEventColor: UIColor? = #colorLiteral(red: 0.9593754411, green: 0.2246205509, blue: 0.2793699503, alpha: 1)
+    public var cellSelectionType: CalendarStyle.CellSelectionType? = .single
+    public var cellShape : CalendarStyle.CellShapeOptions? = .round
+    public var cellBackgroundColor : UIColor? = #colorLiteral(red: 0.9593754411, green: 0.2246205509, blue: 0.2793699503, alpha: 1)
+    public var cellTodayBackgroundColor : UIColor? = #colorLiteral(red: 0.9593754411, green: 0.2246205509, blue: 0.2793699503, alpha: 1)
+    public var cellBorderColor : UIColor? = #colorLiteral(red: 0.9593754411, green: 0.2246205509, blue: 0.2793699503, alpha: 1)
+    public var headerTextColor : UIColor? = #colorLiteral(red: 0.9593754411, green: 0.2246205509, blue: 0.2793699503, alpha: 1)
+    public var cellTextColor : UIColor? = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    public var cellTodayTextColor : UIColor? = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     
     override public func viewDidLoad() {
         super.viewDidLoad()
@@ -34,16 +36,16 @@ public class CalendarViewVC: UIViewController, CalendarViewDelegate {
         
         let today = Date()
         calendarView.setCurrentDate(date: today, animated: false)
-        
-        CalendarStyle.cellEventColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-        CalendarStyle.cellSelectionType = .single
-        CalendarStyle.cellShape = .round
-        CalendarStyle.cellBackgroundColor = UIColor.clear
-        CalendarStyle.cellTodayBackgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
-        CalendarStyle.cellBorderColor = #colorLiteral(red: 0.1664928794, green: 0.7488424182, blue: 0.8295294642, alpha: 1)
-        CalendarStyle.headerTextColor = UIColor.black
-        CalendarStyle.cellTextColor = UIColor.black
-        CalendarStyle.cellTodayTextColor = UIColor.white
+
+        CalendarStyle.cellEventColor = cellEventColor!
+        CalendarStyle.cellSelectionType = cellSelectionType!
+        CalendarStyle.cellShape = cellShape!
+        CalendarStyle.cellBackgroundColor = cellBackgroundColor!
+        CalendarStyle.cellTodayBackgroundColor = cellTodayBackgroundColor!
+        CalendarStyle.cellBorderColor = cellBorderColor!
+        CalendarStyle.headerTextColor = headerTextColor!
+        CalendarStyle.cellTextColor = cellTextColor!
+        CalendarStyle.cellTodayTextColor = cellTodayTextColor!
     }
     
     func setupUI() {
@@ -51,44 +53,64 @@ public class CalendarViewVC: UIViewController, CalendarViewDelegate {
         self.calendarView = CalendarView(frame: .zero)
         self.okButton = UIButton()
         self.cancelButton = UIButton()
+        self.stackView = UIStackView()
         
         okButton.setTitle("Ok", for: .normal)
         cancelButton.setTitle("Cancel", for: .normal)
         
-        okButton.setTitleColor(UIColor.black, for: .normal)
-        cancelButton.setTitleColor(UIColor.black, for: .normal)
+        okButton.setTitleColor(UIColor.white, for: .normal)
+        cancelButton.setTitleColor(UIColor.white, for: .normal)
         
         okButton.addTarget(self, action: #selector(handleTap), for: .touchUpInside)
         cancelButton.addTarget(self, action: #selector(handleTap), for: .touchUpInside)
         
-        self.view.addSubview(calendarView)
-        self.view.addSubview(okButton)
-        self.view.addSubview(cancelButton)
+        cancelButton.titleLabel?.textAlignment = .center
+        okButton.titleLabel?.textAlignment = .center
         
+        stackView.distribution = .fillEqually
+        
+        cancelButton.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        okButton.backgroundColor = #colorLiteral(red: 0.12773031, green: 0.6113714576, blue: 0.9892446399, alpha: 1)
+        
+        self.view.addSubview(calendarView)
+        self.view.addSubview(stackView)
+        self.stackView.addArrangedSubview(okButton)
+        self.stackView.addArrangedSubview(cancelButton)
+
+        self.setupConstraints()
+    }
+    
+    func setupConstraints() {
         calendarView.translatesAutoresizingMaskIntoConstraints = false
-        okButton.translatesAutoresizingMaskIntoConstraints = false
-        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         
         calendarView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 40).isActive = true
         calendarView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -120).isActive = true
         calendarView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
         calendarView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
         
-        okButton.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 8).isActive = true
-        okButton.leadingAnchor.constraint(equalTo: calendarView.leadingAnchor).isActive = true
-        okButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
-        
-        cancelButton.topAnchor.constraint(equalTo: self.calendarView.bottomAnchor, constant: 8).isActive = true
-        cancelButton.trailingAnchor.constraint(equalTo: self.calendarView.trailingAnchor).isActive = true
-        cancelButton.widthAnchor.constraint(equalToConstant: 110).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.calendarView.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.calendarView.trailingAnchor).isActive = true
+        stackView.topAnchor.constraint(equalTo: self.calendarView.bottomAnchor).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
     
     public init() {
         super.init(nibName: nil, bundle: nil)
     }
     
-    convenience init(cellEventColor: UIColor, cellSelectionType: CalendarStyle.CellSelectionType, cellShape: CalendarStyle.CellShapeOptions, cellBackgroundColor: UIColor, cellTodayBackgroundColor: UIColor, cellBorderColor: UIColor, headerTextColor: UIColor, cellTextColor: UIColor, cellTodayTextColor: UIColor) {
+    convenience public init(cellEventColor: UIColor?, cellSelectionType: CalendarStyle.CellSelectionType?, cellShape: CalendarStyle.CellShapeOptions? , cellBackgroundColor: UIColor?, cellTodayBackgroundColor: UIColor? , cellBorderColor: UIColor?, headerTextColor: UIColor?, cellTextColor: UIColor?, cellTodayTextColor: UIColor?) {
         self.init()
+
+        self.cellEventColor = cellEventColor
+        self.cellSelectionType = cellSelectionType
+        self.cellShape = cellShape
+        self.cellBackgroundColor = cellBackgroundColor
+        self.cellTodayBackgroundColor = cellTodayBackgroundColor
+        self.cellBorderColor = cellBorderColor
+        self.headerTextColor = headerTextColor
+        self.cellTextColor = cellTextColor
+        self.cellTodayTextColor = cellTodayTextColor
     }
     
     required public init?(coder aDecoder: NSCoder) {
--- Example/ALCalendar/Demo/Controllers/CalendarPicker.swift
+++ Example/ALCalendar/Demo/Controllers/CalendarPicker.swift
@@ -36,13 +36,13 @@ class CalendarPicker: UIViewController {
     }
     
     @IBAction func testButtonPressed(_ sender: Any) {
-        let vc = CalendarViewVC()
+        let vc = CalendarViewVC(cellEventColor: #colorLiteral(red: 0.12773031, green: 0.6113714576, blue: 0.9892446399, alpha: 1), cellSelectionType: .single, cellShape: .square, cellBackgroundColor: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), cellTodayBackgroundColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), cellBorderColor: #colorLiteral(red: 0.9593754411, green: 0.2246205509, blue: 0.2793699503, alpha: 1), headerTextColor: #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1), cellTextColor: #colorLiteral(red: 0.12773031, green: 0.6113714576, blue: 0.9892446399, alpha: 1), cellTodayTextColor: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1))
+        
         vc.view.superview?.frame.size = CGSize(width: 300, height: 350)
         vc.modalPresentationStyle = .overCurrentContext
         vc.modalTransitionStyle = .crossDissolve
         vc.view.backgroundColor = UIColor.white
         present(vc, animated: true, completion: nil)
-        print("dupex")
     }
     
 }
